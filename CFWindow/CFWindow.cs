6e60e6f319ad15fa7a181d9df14fb6bcb9671a3d
Henry Hunt <henryhunt.dev@gmail.com>
2019-12-28 23:45:52 +0000

    Fixed sizing issues (except height) but no minimise/restore animations

--- CFWindow/CFWindow.cs
+++ CFWindow/CFWindow.cs
@@ -1,22 +1,17 @@
-﻿using Microsoft.Win32;
-using System;
+﻿using System;
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
 using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Shell;
-using static CFWindow.Helpers;
-using Screen = System.Windows.Forms.Screen;
 
 namespace CFWindow
 {
     public partial class CFWindow : Window
     {
         private IntPtr WindowHandle;
-        //private Helpers.MONITORINFO CurrentDisplay;
-        private Screen CScreen;
+        private Point MaximisePosition;
 
         private Border WindowFrame;
         private Border WindowContent;
@@ -28,8 +23,6 @@ namespace CFWindow
         private WindowButton WindowRestore;
         private WindowButton WindowClose;
 
-        private bool DragFromMaximised = false;
-
         public CFWindow()
         {
             DataContext = this;
@@ -40,15 +33,15 @@ namespace CFWindow
 
         public override void OnApplyTemplate()
         {
-            WindowFrame = GetTemplateChild<Border>("WindowFrame");
-            WindowContent = GetTemplateChild<Border>("WindowContent");
-            WindowIcon = GetTemplateChild<Image>("WindowIcon");
-            WindowTitle = GetTemplateChild<TextBlock>("WindowTitle");
-            WindowButtons = GetTemplateChild<StackPanel>("WindowButtons");
-            WindowMinimise = GetTemplateChild<WindowButton>("WindowMinimize");
-            WindowMaximise = GetTemplateChild<WindowButton>("WindowMaximize");
-            WindowRestore = GetTemplateChild<WindowButton>("WindowRestore");
-            WindowClose = GetTemplateChild<WindowButton>("WindowClose");
+            WindowFrame = (Border)GetTemplateChild("WindowFrame");
+            WindowContent = (Border)GetTemplateChild("WindowContent");
+            WindowIcon = (Image)GetTemplateChild("WindowIcon");
+            WindowTitle = (TextBlock)GetTemplateChild("WindowTitle");
+            WindowButtons = (StackPanel)GetTemplateChild("WindowButtons");
+            WindowMinimise = (WindowButton)GetTemplateChild("WindowMinimize");
+            WindowMaximise = (WindowButton)GetTemplateChild("WindowMaximize");
+            WindowRestore = (WindowButton)GetTemplateChild("WindowRestore");
+            WindowClose = (WindowButton)GetTemplateChild("WindowClose");
 
             ApplyWindowState();
 
@@ -68,36 +61,14 @@ namespace CFWindow
             }
 
             // Add event handlers here as they need the visual tree
-            SizeChanged += CFWindow_SizeChanged;
             StateChanged += CFWindow_StateChanged;
             Activated += CFWindow_ActiveChanged;
             Deactivated += CFWindow_ActiveChanged;
-            LocationChanged += CFWindow_LocationChanged;
 
             WindowMinimise.Click += WindowMinimize_Click;
             WindowMaximise.Click += WindowMaximize_Click;
             WindowRestore.Click += WindowRestore_Click;
             WindowClose.Click += WindowClose_Click;
-
-            SystemEvents.DisplaySettingsChanged += DisplaySettingsChanged;
-        }
-
-        private void CFWindow_LocationChanged(object sender, EventArgs e)
-        {
-            //CurrentDisplay = Helpers.GetWindowDisplay(WindowHandlePtr);
-
-            //PresentationSource source = PresentationSource.FromVisual(this);
-            //double dpiX = 0, dpiY = 0;
-            //if (source != null)
-            //{
-            //    dpiX = 96.0 * source.CompositionTarget.TransformToDevice.M11;
-            //    dpiY = 96.0 * source.CompositionTarget.TransformToDevice.M22;
-            //}
-
-            //Console.WriteLine(dpiX + ", " + dpiY);
-
-            CScreen = Screen.FromHandle(WindowHandle);
-            //Console.WriteLine("{0} working: {1} left: {2} top: {3}", CScreen.DeviceName, CScreen.WorkingArea, Left, Top);
         }
 
         protected override void OnSourceInitialized(EventArgs e)
@@ -107,41 +78,45 @@ namespace CFWindow
             HwndSource windowHandle = HwndSource.FromHwnd(WindowHandle);
             if (windowHandle != null)
                 windowHandle.AddHook(new HwndSourceHook(WindowProcedure));
-
-            // Get the OS-implemented window style information
-            uint windowStyle = (uint)Helpers.GetWindowLongPtr(WindowHandle, Helpers.GWL_STYLE);
-
-            // Remove the OS-implemented window border. If there's no border then Windows will not maximise the window
-            // larger than the screen to hide it, and there is then no need for any hacks to re-align the window. This
-            // is the crucial key to solving all of the sizing issues associated with custom window styling.
-            // Based on https://stackoverflow.com/questions/2398746/removing-window-border
-            windowStyle &= ~(Helpers.WS_THICKFRAME | Helpers.WS_CAPTION | Helpers.WS_SYSMENU);
-            Helpers.SetWindowLongPtr(new HandleRef(this, WindowHandle), Helpers.GWL_STYLE, (IntPtr)windowStyle);
         }
-
         private IntPtr WindowProcedure(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam, ref bool handled)
         {
             switch ((uint)msg)
             {
-                // Override non-client area right click to show the system menu under the window icon
-                case Helpers.WM_NCRBUTTONUP:
+                // The OS maximises windows by making them slightly larger than the screen in order to hide the resize
+                // borders. This message provides the default maximise position, which if negative tells us how big the
+                // overhang is (allowing it to be negated later on)
+                case Helpers.WM_GETMINMAXINFO:
                     {
-                        handled = true;
-                        ShowSystemMenu();
-                        break;
-                    }
+                        Helpers.MINMAXINFO minMaxInfo = (Helpers.MINMAXINFO)Marshal.PtrToStructure(lparam,
+                            typeof(Helpers.MINMAXINFO));
 
-                // Show the system menu when the alt+space shortcut is triggered
-                case Helpers.WM_KEYUP:
-                    {
-                        if ((int)wparam == Helpers.VK_MENU)
-                        {
-                            handled = true;
-                            ShowSystemMenu();
-                        }
+                        MaximisePosition = new Point(minMaxInfo.ptMaxPosition.X, minMaxInfo.ptMaxPosition.Y);
+                        if (WindowState == WindowState.Maximized)
+                            ApplyWindowState();
 
                         break;
                     }
+
+                    //// Override non-client area right click to show the system menu under the window icon
+                    //case Helpers.WM_NCRBUTTONUP:
+                    //    {
+                    //        handled = true;
+                    //        ShowSystemMenu();
+                    //        break;
+                    //    }
+
+                    //// Show the system menu when the alt+space shortcut is triggered
+                    //case Helpers.WM_KEYUP:
+                    //    {
+                    //        if ((int)wparam == Helpers.VK_MENU)
+                    //        {
+                    //            handled = true;
+                    //            ShowSystemMenu();
+                    //        }
+
+                    //        break;
+                    //    }
             }
 
             return IntPtr.Zero;
@@ -164,8 +139,7 @@ namespace CFWindow
             menuYPosition += WindowIcon.Height;
 
             // Calculate position to show menu at (bottom-left corner of icon)
-            Point point = new Point((Left + menuXPosition) * 2, (Top + menuYPosition) * 2);
-
+            Point point = Helpers.TransformToPixels2(Left, Top);
             IntPtr menu = Helpers.GetSystemMenu(WindowHandle, false);
 
             // Show the menu and block until dismissed or a command is selected
@@ -181,26 +155,6 @@ namespace CFWindow
         private void CFWindow_StateChanged(object sender, EventArgs e)
         {
             ApplyWindowState();
-
-            //Console.WriteLine("state");
-            //SetWindowLongPtr(new HandleRef(this, WindowHandlePtr), GWL_STYLE, new IntPtr(WS_VISIBLE));
-
-            //SetWindowPos(WindowHandlePtr, new IntPtr(0), 10, 10, 0, 0, (SetWindowPosFlags)
-            //    (SWP_NOZORDER | SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE | SWP_FRAMECHANGED));
-        }
-        private void CFWindow_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            //if (WindowState != WindowState.Maximized) return;
-
-            //Screen screen = Screen.FromHandle(new WindowInteropHelper(this).Handle);
-
-            // Window has changed screen while maximised
-            //if (PreviousScreen.Width != screen.WorkingArea.Width
-            //    || PreviousScreen.Height != screen.WorkingArea.Height)
-            //{
-            //    PreviousScreen = new Size(screen.WorkingArea.Width, screen.WorkingArea.Height);
-            //    OnStateChanged(new EventArgs());
-            //}
         }
         private void CFWindow_ActiveChanged(object sender, EventArgs e)
         {
@@ -228,12 +182,6 @@ namespace CFWindow
         private void WindowClose_Click(object sender, RoutedEventArgs e)
         {
             Close();
-        }
-
-        private void DisplaySettingsChanged(object sender, EventArgs e)
-        {
-            //CurrentDisplay = Helpers.GetWindowDisplay(WindowHandlePtr);
-            OnStateChanged(new EventArgs());
         }
 
         private void ApplyWindowState()
@@ -268,6 +216,9 @@ namespace CFWindow
                     GlassFrameThickness = new Thickness(0),
                     CornerRadius = new CornerRadius(0)
                 });
+
+                Point corrected = Helpers.TransformToPixels(-MaximisePosition.X, -MaximisePosition.Y);
+                WindowFrame.BorderThickness = new Thickness(corrected.X, corrected.Y, corrected.X, corrected.Y);
             }
             else if (WindowState == WindowState.Normal)
             {
@@ -301,6 +252,8 @@ namespace CFWindow
                     GlassFrameThickness = new Thickness(0),
                     CornerRadius = new CornerRadius(0)
                 });
+
+                WindowFrame.Margin = new Thickness(0);
             }
         }
         private void ApplyWindowActive()
@@ -335,11 +288,6 @@ namespace CFWindow
                 WindowContent.BorderBrush = (SolidColorBrush)Application
                     .Current.Resources["Window.Inactive.Content.BorderBrush"];
             }
-        }
-
-        private T GetTemplateChild<T>(string childName) where T : DependencyObject
-        {
-            return (T)GetTemplateChild(childName);
         }
     }
 }
